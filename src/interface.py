29da6ea4fa80df5753179df67722cd395015df4b
Kyle Remmenga <kyleremmenga@gmail.com>
2024-04-16 18:13:22 +0000

    added interface module

--- src/interface.py
+++ src/interface.py
@@ -4,6 +4,7 @@ Module to handle the input and player turns
 
 from board import Board, FullError
 
+
 class Interface():
 
     def __init__(self) -> None:
@@ -13,7 +14,7 @@ class Interface():
         self._player_turn: int = 1
         self._turn_count: int = 0
         self.board: Board = Board()
-    
+
     def print_welcome(self) -> None:
         """
         function to print the welcome message for the game
@@ -24,8 +25,8 @@ class Interface():
         """
         function to print the turn instructions for the players
         """
-        print(f"Its Player {self._player_turn}'s turn!
-              Please enter the column number to drop a piece.")
+        print(f"""Its Player {self._player_turn}'s turn!
+              Please enter the column number to drop a piece.""")
 
     def _switch_player(self) -> None:
         """
@@ -35,7 +36,7 @@ class Interface():
             self._player_turn = 2
         else:
             self._player_turn = 1
-        
+
     @property
     def player_turn(self) -> int:
         """
@@ -45,7 +46,7 @@ class Interface():
             int: the integer representing what player's turn it is
         """
         return self._player_turn
-    
+
     def _read_input(self) -> int:
         """
         function to read the input from the players
--- src/main.py
+++ src/main.py
@@ -7,8 +7,9 @@ def draw(board: Board) -> None:
     # draw background
     for x in range(8):
         for y in range(8):
-            player_number = board.get_player_at_spot(x, y)
+            #player_number = board.get_player_at_spot(x, y)
             # draw a circle of the correct color at the relavent location
+            pass
 
 
 def main() -> None:
