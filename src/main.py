f6889b4d4b2e7b56db89c508fc79152f151d7a4c
Kyle Remmenga <kyleremmenga@gmail.com>
2024-04-16 18:15:20 +0000

    added interface module

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def draw(board: Board) -> None:
     # draw background
     for x in range(8):
         for y in range(8):
-            #player_number = board.get_player_at_spot(x, y)
+            # player_number = board.get_player_at_spot(x, y)
             # draw a circle of the correct color at the relavent location
             pass
 
