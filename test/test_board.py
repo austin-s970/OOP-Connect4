7db36fbd62959d9a5fbd57f648a2e8a68a3e2371
Austin Saylor <austin@saylors.com>
2024-04-20 00:57:37 +0000

    temporary adjustment

--- test/test_board.py
+++ test/test_board.py
@@ -1,5 +1,5 @@
 import unittest
-from hypothesis import given, strategies, assume
+from hypothesis import given, settings, strategies, assume
 
 import board
 
@@ -60,11 +60,11 @@ class TestBoard(unittest.TestCase):
         self.assertIsInstance(self.board._board[y][x], board.Spot)
 
     # No deadline was a temporary change to satisfy CI/CD
+    @settings(deadline=None)
     @given(width=strategies.integers(1, 100),
            height=strategies.integers(1, 100),
            x=strategies.integers(0, 100),
-           y=strategies.integers(0, 100),
-           deadline=None)
+           y=strategies.integers(0, 100))
     def test_init_args(self, width: int, height: int, x: int, y: int) -> None:
         assume(x < width)
         assume(y < height)
